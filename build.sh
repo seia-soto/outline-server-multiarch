b705a5a945bbebe6498a14aab25f18e35393cd4d
seia-soto <seia@outlook.kr>
2022-01-20 18:34:22 +0900

    fix: add nvm install command

--- build.sh
+++ build.sh
@@ -83,6 +83,8 @@ fi
 git checkout "${CHECKPOINT}"
 
 # Load NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
